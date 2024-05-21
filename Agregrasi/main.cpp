54ac546e97600ad1ad4bdb2407851aaf1fe31fe2
nabilasaf <nabilasafitri861@gmail.com>
2024-05-21 16:14:32 +0700

    menampilkan nama anak

--- Agregrasi/main.cpp
+++ Agregrasi/main.cpp
@@ -9,6 +9,8 @@ int main() {
 
     ibu* varIbu = new ibu ("dini");
     ibu* varIbu2 = new ibu("novi");
+    anak* varAnak1 = new anak ("tono");
+    anak* varAnak2 = new anak ("rini");
+    anak* varAnak3 = new anak ("Dewi");
 
-    
 }
