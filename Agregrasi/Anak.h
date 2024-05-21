cc62ffd9f4cabbaf826fd3f009c02da927a11fd0
nabilasaf <nabilasafitri861@gmail.com>
2024-05-21 15:53:33 +0700

    membenarkan string pada kelas

--- Agregrasi/Anak.h
+++ Agregrasi/Anak.h
@@ -4,7 +4,7 @@
 class anak {
     public:
       string nama;
-      anak(stringpNama) :nama(pNama) {
+      anak(string pNama) :nama(pNama) {
         cout << "Anak \" " << nama << "\" ada\n";
       }
       ~anak() {
